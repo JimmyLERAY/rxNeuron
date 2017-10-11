61c0236cb6a28f7cbc7c38cbfedd6a9448270904
Jimmy LERAY <jimmy.leray@zenika.com>
2017-10-12 01:09:32 +0200

    subscribe method

--- Neuron.js
+++ Neuron.js
@@ -1,15 +1,16 @@
 const Rx = require('rxjs')
 
 module.exports = class Neuron {
-    constructor(synapses, callback) {
+    constructor(inputs) {
         this.output = Rx.ConnectableObservable.create(observer => { this.observer = observer })
-        this.output.subscribe(callback)
     
-        this.weights = new Array(synapses.length).fill().map(val => Math.random())
-        this.synapses = synapses.map((synapse, i) => synapse.map(val => val * this.weights[i]))
-        this.core = Rx.Observable.merge(...this.synapses).bufferTime(2)
+        this.weights = new Array(inputs.length).fill().map(val => Math.random())
+        this.inputs = inputs.map((input, i) => input.map(val => val * this.weights[i]))
+        this.core = Rx.Observable.merge(...this.inputs).bufferTime(5)
             .map(buffer => buffer.reduce((sum, val) => sum + val, 0))
-            .filter(val => val > 0.5 * synapses.length)
+            .filter(val => val > 0.5 * inputs.length)
             .subscribe(val => { this.observer.next(1) })
     }
+
+    subscribe(callback) { this.output.subscribe(callback) }
 }
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Rx = require('rxjs')
 const Neuron = require('./Neuron')
 
-let neuron1 = new Neuron([
+let neuron = new Neuron([
     Rx.ConnectableObservable.interval(11).map(val => 1),
     Rx.ConnectableObservable.interval(19).map(val => 1),
     Rx.ConnectableObservable.interval(31).map(val => 1)
-], val => console.log(1))
+])
+
+neuron.subscribe(val => console.log(1))
