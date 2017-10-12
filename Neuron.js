91ee8008acd03ffa7525c4b8b122f31d5475f794
LERAY Jimmy <jleray@jouve.com>
2017-10-12 14:34:56 +0200

    add some logs

--- Neuron.js
+++ Neuron.js
@@ -12,12 +12,12 @@ module.exports = class Neuron {
         // Create a connectableObservable then connect it
         // Other neurons can already connect to this hot source
         this.output = Rx.Observable.create(observer => { this.observer = observer }).publish()
-        this.output.connect()
+        this.output.connect(); console.log('Output published')
     }
 
     resetCore() {
         // unsubscribe from the previous core if defined
-        if (this.core) this.core.unsubscribe()
+        if (this.core) { this.core.unsubscribe(); console.log('Core disconnected') }
 
         // subscribe to the new core which merge, buffer and filter
         // all hot synapses sources into one observable
@@ -26,23 +26,25 @@ module.exports = class Neuron {
             .bufferTime(5) .map(buffer => buffer.reduce((sum, val) => sum + val, 0))
             .filter(val => val > 0.5 * this.synapses.length)
             .subscribe(() => { this.observer.next(1) })
+            console.log('Core connected')
     }
 
     addSyn(inputs) {
         // Add new references to inputs observables then reset the core source
         this.synapses.push(...inputs.map(input => { return { observable: input, weigth: Math.random() } }))
-        console.log(`${ inputs.length } new synapse(s) have been connected to the core`)
+        console.log(`${ inputs.length } new synapse(s) connected`)
         this.resetCore()
     }
 
     remSyn(idxs) {
         // Remove some references to inputs observables then reset the core source
         idxs.sort((a,b) => b-a).forEach(idx => { this.synapses.splice(idx, 1) })
-        console.log(`${ idxs.length } synapse(s) have been disconnected to the core`)
+        console.log(`${ idxs.length } synapse(s) disconnected`)
         this.resetCore()
     }
 
     subscribe(callback) {
+        console.log('Output new subscription')
         // Return the subscribe object of output
         return this.output.subscribe(callback)
     }
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Rx = require('rxjs')
 const Neuron = require('./Neuron')
 
-
 // Instance of Neuron
 const neuron = new Neuron()
 neuron.subscribe()
@@ -11,7 +10,6 @@ const input1 = Rx.Observable.interval(11).map(val => 1).publish()
 const input2 = Rx.Observable.interval(19).map(val => 1).publish()
 const input3 = Rx.Observable.interval(31).map(val => 1).publish()
 input1.connect(); input2.connect(); input3.connect()
-
 
 // Connect these sources then discconnect some
 neuron.addSyn([input1, input2, input3])
